ef4e65be7b4b399b3e4f3dd87c3b27de65ebe4d1
igor <x.firedragon@gmail.com>
2020-01-12 23:13:25 +0500

    init users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
