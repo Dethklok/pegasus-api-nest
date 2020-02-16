f6524e89ea0b5682ee2b778a831d1be814067588
igor <x.firedragon@gmail.com>
2020-02-16 15:17:26 +0500

    Auth module: documentation for signin and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,27 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ApiTags, ApiCreatedResponse, ApiBadRequestResponse, ApiUnauthorizedResponse, ApiConflictResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
   ) {}
 
+  @ApiCreatedResponse({ description: 'Token successfully created.' })
+  @ApiBadRequestResponse({ description: 'Bad request. Invalid data in DTO.' })
+  @ApiUnauthorizedResponse({ description: 'Credentials incorrect.' })
   @Post('signin')
   signin(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<string> {
     return this.authService.signin(authCredentialsDto);
   }
 
+  @ApiCreatedResponse({ description: 'User and token successfully created.' })
+  @ApiBadRequestResponse({ description: 'Bad request. Invalid data in DTO.' })
+  @ApiConflictResponse({ description: 'Username already exists.' })
   @Post('signup')
   signup(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<string> {
     return this.authService.signup(createUserDto);
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthCredentialsDto {
+  @ApiProperty()
   @IsString()
   readonly username: string;
 
+  @ApiProperty()
   @IsString()
   readonly password: string;
 }
